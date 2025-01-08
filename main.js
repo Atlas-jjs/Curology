2dfceb7b586a92b5583b8dc3dfb567eda7818d67
jjgobas <jjgobas@gmail.com>
2025-01-08 15:39:53 +0800

    Code customer testimonials

--- main.js
+++ main.js
@@ -13,3 +13,30 @@ document.addEventListener('click', e => {
             hamburgerMenu.classList.remove("active");
         }
 });
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 32,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+      slidesPerView: 2
+    }
+  }
+});
